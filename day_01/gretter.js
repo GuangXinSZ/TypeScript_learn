8bdffbedc075d53cc3bf2f717510baae3015d6b4
xin <1446854524@qq.com>
2018-07-08 01:56:41 +0800

    ts

--- day_01/gretter.js
+++ day_01/gretter.js
@@ -1,6 +1,18 @@
 //下载 typescript cnpm install -g typescript
+//创建一个类
+var Student = /** @class */ (function () {
+    function Student(firstName, middleInitial, lastName) {
+        this.firstName = firstName;
+        this.middleInitial = middleInitial;
+        this.lastName = lastName;
+        //闭包使用this
+        this.fullName = firstName + " " + middleInitial + " " + lastName;
+    }
+    return Student;
+}());
 function gretter(person) {
-    return "hello" + person;
+    return "hello" + person.firstName + 'lastname' + person.lastName;
 }
-var user = [1, 2, 3];
+// let user = { firstName: "liu",lastName: "xin" }
+var user = new Student("Jane", "xin", "liu");
 document.body.innerHTML = gretter(user);
--- day_01/gretter.ts
+++ day_01/gretter.ts
@@ -1,7 +1,26 @@
 //下载 typescript cnpm install -g typescript
-function gretter(person: string){
-    return "hello" + person
+
+
+//创建一个类
+class Student {
+    //定义一个字符串
+    fullName: string;
+    // constructor就类似数据初始化 类似php
+    constructor( public firstName, public middleInitial, public lastName ) {
+        //闭包使用this
+        this.fullName = firstName + " " + middleInitial + " " + lastName
+     }
 }
-let user = [1,2,3]
+//提供接口
+interface Person {
+    firstName: string;
+    lastName: string;
+}
+function gretter(person: Person){
+    return "hello" + person.firstName + 'lastname' + person.lastName
+}
+// let user = { firstName: "liu",lastName: "xin" }
+
+let user = new Student("Jane", "xin", "liu")
 
 document.body.innerHTML = gretter(user)
