e3e560cb5cb7f2837035c6ad8c0a802ba1d3ef4f
xin <1446854524@qq.com>
2018-07-08 01:23:26 +0800

    ts

---
+++ day_01/gretter.js
@@ -0,0 +1,6 @@
+//下载 typescript cnpm install -g typescript
+function gretter(person) {
+    return "hello" + person;
+}
+var user = [1, 2, 3];
+document.body.innerHTML = gretter(user);
---
+++ day_01/gretter.ts
@@ -0,0 +1,7 @@
+//下载 typescript cnpm install -g typescript
+function gretter(person: string){
+    return "hello" + person
+}
+let user = [1,2,3]
+
+document.body.innerHTML = gretter(user)
